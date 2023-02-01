c7dc03b8460981d344e6ae02258c26d112239e89
Enzo <115178427+Sinoqa@users.noreply.github.com>
2023-02-01 14:11:19 -0800

    Lothar Collatz formula with number of steps

---
+++ Lothar Collatz Formula.py
@@ -0,0 +1,10 @@
+c0 = int(input("> "))
+steps = 0
+while c0 != 1:
+    if c0 % 2 == 0:
+        c0 /= 2
+    elif c0 % 2 != 0:
+        c0 = 3 * c0 +1
+    steps += 1
+    print(int(c0))
+print("steps =  ",steps)
