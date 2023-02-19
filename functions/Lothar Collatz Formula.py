0a3cfda2b8eba0b90040f8ff29be49e5406a4cdb
Enzo <115178427+Sinoqa@users.noreply.github.com>
2023-02-19 12:42:08 -0800

    Update Lothar Collatz Formula.py

--- functions/Lothar Collatz Formula.py
+++ functions/Lothar Collatz Formula.py
@@ -1,14 +1,10 @@
-status = False
-while status != True:
-    def readint(prompt, min, max):
-        try:
-            while prompt != int:
-                if prompt < min or prompt > max:
-                    print(f"Error: The value is not within permitted range {min}..{max}")
-        except ValueError:
-            print("error")
-
-number = int(input(f"Enter a number from -10 to 10 "))
-v = readint(number, -10, 10)
-
-
+c0 = int(input("> "))
+steps = 0
+while c0 != 1:
+    if c0 % 2 == 0:
+        c0 /= 2
+    elif c0 % 2 != 0:
+        c0 = 3 * c0 +1
+    steps += 1
+    print(int(c0))
+print("steps = ",steps)
