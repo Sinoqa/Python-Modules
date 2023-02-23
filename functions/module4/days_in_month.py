f9147c60d1b1d0ff45568e7fad7fc195e6d785ba
Enzo <115178427+Sinoqa@users.noreply.github.com>
2023-02-22 20:40:21 -0800

    Create days_in_month.py

---
+++ functions/module4/days_in_month.py
@@ -0,0 +1,36 @@
+def isYearLeap(year):
+    if year < 1582:
+        return False
+    if year % 4 != 0:
+        return False
+    elif year % 100 != 0:
+        return True
+    elif year % 400 != 0:
+        return False
+    else:
+        return True
+
+days_month = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+def daysInMonth(year, month):
+    year_check = isYearLeap(year)
+    if year_check == True:
+        days_month[2] = 29
+    else:
+        pass
+    return days_month[month]
+
+    
+
+
+testYears = [1900, 2000, 2016, 1987]
+testMonths = [2,2,1,11]
+testResults = [28,29,31,30]
+for i in range (len(testYears)):
+    yr = testYears[i]
+    mo = testMonths[i]
+    print(yr, mo, "=>", end="")
+    result = daysInMonth(yr,mo)
+    if result == testResults[i]:
+        print("ok")
+    else:
+        print("failed")
